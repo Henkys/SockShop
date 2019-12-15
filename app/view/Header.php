f66204ca889f59d75b6741a8024673b1056076d4
JurredeBruin <50706153+JurredeBruin@users.noreply.github.com>
2019-12-15 15:28:22 +0100

    header hero wave

--- app/view/Header.php
+++ app/view/Header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="../app/view/css/styles.css">
+    <link rel="stylesheet" href="../app/view/assets/css/styles.css">
 
     <script src="../app/view/assets/js/app.js"></script>
 
@@ -14,8 +14,18 @@
 <body>
 
 <header>
+    <!-- <div id="wave">
+    </div> -->
     <nav>
-        <h1>Henkys</h1>
+    <h1 class="center">Henkys</h1>
     </nav>
+
+    <section class="bg-default">
+    <div class="section-wave">
+          <svg x="0px" y="0px" width="1920px" height="46px" viewBox="0 0 1920 46" preserveAspectRatio="none">
+            <path d="M1920,0.5c-82.8,0-109.1,44-192.3,44c-78.8,0-116.2-44-191.7-44c-77.1,0-115.9,44-192,44c-78.2,0-114.6-44-192-44c-78.4,0-115.3,44-192,44c-76.9-0.1-119-44-192-44c-77,0-115.2,44-192,44c-73.6,0-114-44-190.9-44c-78.5,0-117.2,44-194.1,44c-75.9,0-113-44-191-44V46h1920V0.5z"></path>
+          </svg>
+        </div>
+    </section>
 </header>
     
