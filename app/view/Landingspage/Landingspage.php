4ec4c3848e5c770a173132ee34da0e26e901eef7
Romy <romyhuzen@gmail.com>
2019-12-18 08:51:01 +0100

    change writing

--- app/view/Landingspage/Landingspage.php
+++ app/view/Landingspage/Landingspage.php
@@ -5,8 +5,8 @@
 <div class="container">
     <div class="row">
         <div class="col-md- herotext">
-        <h4 class="center">make the right choice</h4>
-    <h1 class="center">sockshop</h1>
+        <h4 class="center">Make the right choice</h4>
+    <h1 class="center">Sock-shop</h1>
 </div>
 </div>
 </div>
